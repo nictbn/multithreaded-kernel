dbdbdc3f479d5612587908529c95d7bafe9fe093
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-12 22:52:05 +0200

    Fix for BIOS Parameter Block

--- hello-world-bootloader/boot.asm
+++ hello-world-bootloader/boot.asm
@@ -1,7 +1,12 @@
 ORG 0
 BITS 16
-jmp 0x7c0:start
+_start:
+    jmp short start
+    nop
+times 33 db 0
 start:
+    jmp 0x7c0:step2
+step2:
     cli ; Clear interrupts
     mov ax, 0x7C0
     mov ds, ax
