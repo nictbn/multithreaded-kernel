d951c96613c93d3f4e0113919d1860a245f1d323
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-21 21:18:40 +0200

    Extracts char preparation into a function

--- protected-mode/src/kernel.c
+++ protected-mode/src/kernel.c
@@ -1,8 +1,12 @@
 #include "kernel.h"
 #include <stdint.h>
 
+uint16_t terminal_make_char(char c, char colour) {
+    return (colour << 8) | c;
+}
+
 void kernel_main()
 {
     uint16_t* video_mem = (uint16_t*)(0xB8000);
-    video_mem[0] = 0x0341;
+    video_mem[0] = terminal_make_char('B', 3);
 }
