1945e86fecde698a2b544de2a958e8f6aa9f7085
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-21 21:29:37 +0200

    Adds console clear function

--- protected-mode/src/kernel.c
+++ protected-mode/src/kernel.c
@@ -1,12 +1,22 @@
 #include "kernel.h"
 #include <stdint.h>
 
+uint16_t* video_mem = 0;
+
 uint16_t terminal_make_char(char c, char colour) {
     return (colour << 8) | c;
 }
 
-void kernel_main()
-{
-    uint16_t* video_mem = (uint16_t*)(0xB8000);
-    video_mem[0] = terminal_make_char('B', 3);
+void terminal_initialize() {
+    video_mem = (uint16_t*)(0xB8000);
+    for (int y = 0; y < VGA_HEIGHT; y++) {
+        for (int x = 0; x < VGA_WIDTH; x++) {
+            video_mem[y * VGA_WIDTH + x] = terminal_make_char(' ', 0);
+        }
+    }
+}
+
+void kernel_main() {
+    terminal_initialize();
+    video_mem[0] = terminal_make_char('B', 15);
 }
