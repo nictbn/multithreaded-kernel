a72e4bdcea93633d5dde4f59a1914dce6d2b78e9
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-21 21:13:39 +0200

    Displays character using uint_16 pointer taking endianness into consideration

--- protected-mode/src/kernel.c
+++ protected-mode/src/kernel.c
@@ -1,10 +1,8 @@
 #include "kernel.h"
+#include <stdint.h>
 
 void kernel_main()
 {
-    char* video_mem = (char*)(0xB8000);
-    video_mem[0] = 'A';
-    video_mem[1] = 2;
-    video_mem[2] = 'C';
-    video_mem[3] = 3;
+    uint16_t* video_mem = (uint16_t*)(0xB8000);
+    video_mem[0] = 0x0341;
 }
