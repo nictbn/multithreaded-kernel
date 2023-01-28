ff30d0212a3638aefa6187726ae76b9b01f163d6
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-28 16:35:02 +0200

    Fixes page table allocation

--- protected-mode/src/memory/paging/paging.c
+++ protected-mode/src/memory/paging/paging.c
@@ -7,10 +7,9 @@ static uint32_t* current_directory = 0;
 
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags) {
     uint32_t* directory = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
-
+    int offset = 0;
     for (int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; i++) {
         uint32_t* page_table_entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
-        int offset = 0;
         for (int j = 0; j < PAGING_TOTAL_ENTRIES_PER_TABLE; j++) {
             page_table_entry[j] = (offset + (j * PAGING_PAGE_SIZE)) | flags;
         }
