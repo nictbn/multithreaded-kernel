f900a548dfd14efd8a92f69525e019678f69ed8a
Nicu Taban <nictbndeveloper@gmail.com>
2023-01-29 12:36:03 +0200

    Adds abstractions for the disks

--- protected-mode/src/kernel.c
+++ protected-mode/src/kernel.c
@@ -69,6 +69,9 @@ void kernel_main() {
     // initialize the heap
     kheap_init();
 
+    // search and initialize the disks
+    disk_search_and_init();
+
     // initialize the interrupt descriptor table
     idt_init();
 
@@ -81,10 +84,6 @@ void kernel_main() {
 
     // enable paging
     enable_paging();
-
-    char buf[512];
-    disk_read_sector(0, 1, buf);
-
 
     // enable interrupts
     enable_interrupts();
