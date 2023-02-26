b400cc0a889b8ec9ae82b720396fb8c1828a072b
Nicu Taban <nictbndeveloper@gmail.com>
2023-02-26 16:21:23 +0200

    Implements userland functionality

--- protected-mode/src/task/task.h
+++ protected-mode/src/task/task.h
@@ -42,4 +42,10 @@ struct task* task_new(struct process* process);
 struct task* task_current();
 struct task* task_get_next();
 int task_free(struct task* task);
+void task_run_first_ever_task();
+void task_return(struct registers* registers);
+void restore_general_purpose_registers(struct registers* registers);
+void user_registers();
+int task_switch(struct task* task);
+int task_page();
 #endif
