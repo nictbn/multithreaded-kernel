bcdee1f7adc4bdc9be1e9a2b8901844448c98a0d
Nicu Taban <nictbndeveloper@gmail.com>
2023-03-04 19:07:20 +0200

    Implements the virtual keyboard layer

--- protected-mode/src/kernel.c
+++ protected-mode/src/kernel.c
@@ -19,6 +19,7 @@
 #include "task/task.h"
 #include "task/process.h"
 #include "isr80h/isr80h.h"
+#include "keyboard/keyboard.h"
 
 uint16_t* video_mem = 0;
 uint16_t terminal_row = 0;
@@ -130,6 +131,9 @@ void kernel_main() {
 
     // register the kernel commands
     isr80h_register_commands();
+
+    // initialize all the system keyboards
+    keyboard_init();
 
     struct process* process = 0;
     int res = process_load("0:/blank.bin", &process);
