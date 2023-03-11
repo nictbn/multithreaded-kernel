a92ab77bdca40a05448c57ed03743bbcc2ea72fb
Nicu Taban <nictbndeveloper@gmail.com>
2023-03-11 20:09:33 +0200

    Implements itoa

--- protected-mode/programs/blank/blank.c
+++ protected-mode/programs/blank/blank.c
@@ -2,6 +2,7 @@
 #include "stdlib.h"
 int main (int argc, char** argv) {
     print("Hello, how are you?\n");
+    print(itoa(8763));
     void* ptr = malloc(512);
     free(ptr);
 
--- protected-mode/programs/stdlib/src/stdlib.c
+++ protected-mode/programs/stdlib/src/stdlib.c
@@ -8,3 +8,27 @@ void* malloc(size_t size) {
 void free(void* ptr) {
     return os_free(ptr);
 }
+
+char* itoa(int i) {
+    static char text[12];
+    int loc = 11;
+    text[11] = 0;
+    char negative = 1;
+    if (i >= 0) {
+        negative = 0;
+        i = -i;
+    }
+    while(i) {
+        text[--loc] = '0' - (i % 10);
+        i /= 10;
+    }
+
+    if (loc == 11) {
+        text[--loc] = '0';
+    }
+
+    if (negative) {
+        text[--loc] = '-';
+    }
+    return &text[loc];
+}
