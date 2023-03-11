d8790247e4771208e3586fec22fc9b7c36e8f385
Nicu Taban <nictbndeveloper@gmail.com>
2023-03-11 22:40:52 +0200

    Creates a shell

--- protected-mode/programs/blank/blank.c
+++ protected-mode/programs/blank/blank.c
@@ -3,15 +3,7 @@
 #include "stdio.h"
 int main (int argc, char** argv) {
     printf("The meaning of life and everything is %i\n", 42);
-    print("Hello, how are you?\n");
-    print(itoa(8763));
-    putchar('Z');
-    void* ptr = malloc(512);
-    free(ptr);
     
-    char buf[1024];
-    os_terminal_readline(buf, sizeof(buf), true);
-    print(buf);
     while(1) {}
     return 0;
 }
---
+++ protected-mode/programs/shell/src/shell.c
@@ -0,0 +1,15 @@
+#include "shell.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "os.h"
+
+int main(int argc, char** argv) {
+    print("MyOS V1.0.0\n");
+    while(1) {
+        print(">");
+        char buf[1024];
+        os_terminal_readline(buf, sizeof(buf), true);
+        print("\n");
+    }
+    return 0;
+}
