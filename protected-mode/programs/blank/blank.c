4e085e4796216981f8b316569cf20fc3b7126388
Nicu Taban <nictbndeveloper@gmail.com>
2023-03-12 16:44:47 +0200

    Passing arguments from the C wrapper

--- protected-mode/programs/blank/blank.c
+++ protected-mode/programs/blank/blank.c
@@ -5,10 +5,7 @@
 
 int main (int argc, char** argv) {
     printf("The meaning of life and everything is %i\n", 42);
-
-    struct process_arguments arguments;
-    os_process_get_arguments(&arguments);
-    printf("%i %s\n", arguments.argc, arguments.argv[0]);
+    print(argv[0]);
     while(1) {}
     return 0;
 }
